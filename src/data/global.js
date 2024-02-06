371b95abccff04c1b833d668b65309f7663bf4d8
kidus7 <kidusanteneh7@gmail.com>
2024-02-06 22:21:50 +0300

    changed api url

--- src/data/global.js
+++ src/data/global.js
@@ -1,15 +1,15 @@
-// export const environment = {
-//   URL: 'https://songs-backend-api.onrender.com/api/',
-//   File_URL: 'https://songs-backend-api.onrender.com',
-//   image_url: 'https://songs-backend-api.onrender.com/images/',
-//   video_url: 'https://songs-backend-api.onrender.com/movies/',
-//   Socket_Url: 'https://songs-backend-api.onrender.com'
-// };
-
 export const environment = {
-    URL: 'http://localhost:4000/api/',
-    File_URL: 'http://localhost:4000',
-    image_url: 'http://localhost:4000/images/',
-    video_url: 'http://localhost:4000/movies/',
-    Socket_Url: 'http://localhost:4000'
-  };
+  URL: 'https://songs-backend-api.onrender.com/api/',
+  File_URL: 'https://songs-backend-api.onrender.com',
+  image_url: 'https://songs-backend-api.onrender.com/images/',
+  video_url: 'https://songs-backend-api.onrender.com/movies/',
+  Socket_Url: 'https://songs-backend-api.onrender.com'
+};
+
+// export const environment = {
+//     URL: 'http://localhost:4000/api/',
+//     File_URL: 'http://localhost:4000',
+//     image_url: 'http://localhost:4000/images/',
+//     video_url: 'http://localhost:4000/movies/',
+//     Socket_Url: 'http://localhost:4000'
+//   };
